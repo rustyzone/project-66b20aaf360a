5cc4b7fc66655c8d6a091b895757d779b96115d2
rustyzone <66030523+rustyzone@users.noreply.github.com>
2023-01-07 16:39:57 +0000

    part-one-video

--- src/firebase.js
+++ src/firebase.js
@@ -6,51 +6,60 @@ var firebaseConfig = {
   projectId: "coupon-finder-xxx",
   storageBucket: "coupon-finder-xxx.appspot.com",
   messagingSenderId: "xxxxx",
-  appId: "1:xxxxx:web:xxxx"
+  appId: "1:xxxxx:web:xxxx",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-
 console.log(firebase);
 
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
-
-  if(msg.command == "fetch"){
+  if (msg.command == "fetch") {
     var domain = msg.data.domain;
     var enc_domain = btoa(domain);
-    firebase.database().ref('/domain/'+enc_domain).once('value').then(function(snapshot){
-      response({type: "result", status: "success", data: snapshot.val(), request: msg});
-    });
-
+    firebase
+      .database()
+      .ref("/domain/" + enc_domain)
+      .once("value")
+      .then(function (snapshot) {
+        response({
+          type: "result",
+          status: "success",
+          data: snapshot.val(),
+          request: msg,
+        });
+      });
   }
 
   //submit coupon data..
-  if(msg.command == "post"){
-
+  if (msg.command == "post") {
     var domain = msg.data.domain;
     var enc_domain = btoa(domain);
     var code = msg.data.code;
     var desc = msg.data.desc;
 
-    try{
-
-      var newPost = firebase.database().ref('/domain/'+enc_domain).push().set({
-        code: code,
-        description: desc
-      });
+    try {
+      var newPost = firebase
+        .database()
+        .ref("/domain/" + enc_domain)
+        .push()
+        .set({
+          code: code,
+          description: desc,
+        });
 
       var postId = newPost.key;
-      response({type: "result", status: "success", data: postId, request: msg});
-
-    }catch(e){
+      response({
+        type: "result",
+        status: "success",
+        data: postId,
+        request: msg,
+      });
+    } catch (e) {
       console.log("error:", e);
-      response({type: "result", status: "error", data: e, request: msg});
-
+      response({ type: "result", status: "error", data: e, request: msg });
     }
   }
 
   return true;
-
-
-})
+});
